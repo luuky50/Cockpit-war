a6c45575e54ca2bb64bf85df82546a2e84cc02cf
Thomoviz <66361278+Thomoviz@users.noreply.github.com>
2021-11-30 12:13:31 +0100

    Shooting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,17 +7,23 @@ public class Shooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPreFab;
 
-    public float bulletForce = 50f;
+    public float bulletForce = 30f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) {
+        PlaneInfo planeInfo = GetComponent<PlaneInfo>();
+        if (!planeInfo.isPlayer2 && Input.GetKeyDown(KeyCode.W)) {
+            Shoot();
+        } else if (planeInfo.isPlayer2 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
             Shoot();
         }
     }
 
     void Shoot() {
-        
+        GameObject bullet = Instantiate(bulletPreFab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rigidbody2D = bullet.GetComponent<Rigidbody2D>();
+        rigidbody2D.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 }
