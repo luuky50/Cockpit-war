44e082a83867bee88519c4e5fb1e08e50880eeeb
luuky50 <luukhulshof@hotmail.nl>
2021-12-07 12:26:32 +0100

    Updated gitignore

---
+++ Assets/Scripts/Luuk/PowerUp.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public enum Power
+    {
+        Speed,
+        RapidFire,
+        Shield,
+        Turning
+    }
+
+    public Power power;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Colliding with: " + collision);
+        if (collision.gameObject.tag == "Player")
+        {
+
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if(power)
+    }
+    // Update is call1ed once per frame
+    void Update()
+    {
+        
+    }
+}
